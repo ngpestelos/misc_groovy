37ecf2586054e974decd36e86a9fee00bc7791e9
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-08-21 15:04:34 +0800

    Spread operator and simulating kwargs

---
+++ src/method/Spread.groovy
@@ -0,0 +1,18 @@
+def foo(a, b, c) {
+  println "Foo got $a $b $c"
+}
+
+def bar(params, name) {
+  println "Bar got params=$params, name=$name"
+}
+
+def baz(name, params, conditions) {
+  println "Baz got name=$name params=$params conditions=$conditions"
+}
+
+foo(1, 2, 3)
+foo(*[4,5,6])
+
+bar(one: 'e', bee: 'a', 'abc')
+
+baz('abc', category: 'Guns', id: 123, 'where category = ?')
