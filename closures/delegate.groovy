e88be023760c647b81fa08d6efb400169d026a49
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2009-09-06 14:28:48 +0800

    define a closure within a closure

--- closures/delegate.groovy
+++ closures/delegate.groovy
@@ -9,4 +9,13 @@ examiningClosure() {
   println "owner is " + owner + ", super: " + owner.getClass().superclass.name
   println "delegate is " + delegate +
     ", super:" + delegate.getClass().superclass.name
+
+  examiningClosure() {
+    println "In Closure within the First Closure:"
+    println "class is " + getClass().name
+    println "this is " + this + ", super: " + this.getClass().superclass.name
+    println "owner is " + owner + ", super: " + owner.getClass().superclass.name
+    println "delegate is " + delegate +
+      ", super: " + delegate.getClass().superclass.name
+  }
 }
